04e55259a7e798fa2144221758a6fd989ca6f0cf
Felipe Ramirez <felipe.ramirezrivera@gmail.com>
2022-05-24 08:46:58 -0500

    Tried to make the division working but is not working so far

---
+++ Calculator/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+_
+/src/main/java/Calculator/MultiplyInterface.java,0/5/057df486ed6790950685e24b5f70b03851df3653
+]
+-src/main/java/Calculator/DivideInterface.java,a/b/ab66151584326e5e9ab06c83dd53deb27f9b72b6
+`
+0src/main/java/Calculator/SubstractInterface.java,1/8/184ea162bb6f0c55fc43f92f74e674f9145f92aa
+Z
+*src/main/java/Calculator/SumInterface.java,7/7/77eb85b3ff603000a9260282591db33969f9d6b8
+X
+(src/main/java/Calculator/Calculator.java,2/0/2071a56ea40ce1f9a022d7eeaad13bf675b04e83
--- Calculator/src/main/java/Calculator/Calculator.java
+++ Calculator/src/main/java/Calculator/Calculator.java
@@ -10,19 +10,18 @@ public class Calculator{
     public static final SubstractInterface substractFunction = (a, b) -> a-b;
 
     public static final MultiplyInterface multiplyFunction = (a, b) -> IntStream.iterate(0, x -> sumFunction.sum(x, a)).limit(Math.abs(b)+1).reduce((num1, num2) -> num2).getAsInt();
-    public static final DivideInterface divideFunction = (a,b) -> a==0 ? 0 : b==1 ? a : b==0 ? -1 :IntStream.rangeClosed(0,a).reduce((x,y) -> substractFunction.substract(a,x)>=0 ? sumFunction.sum(x,b) : 0);
+    public static final DivideInterface divideFunction = (a,b) -> a==0 ? 0 : b==1 ? a : b==0 ? -1 : IntStream.rangeClosed(0,a).reduce((x,y) -> substractFunction.substract(a,x)>=0 ? sumFunction.sum(x,b) : y).getAsInt();
+
 
 
 
 
     public static void main(String[] args) {
-//        System.out.println(sumFunction.sum(1,3));
-//        System.out.println(substractFunction.substract(3,3));
-//        System.out.println(multiplyFunction.multiply(6,-6));
+        System.out.println(sumFunction.sum(1,3));
+        System.out.println(substractFunction.substract(3,3));
+        System.out.println(multiplyFunction.multiply(6,-6));
         System.out.println(divideFunction.divide(10,2));
-        System.out.println(IntStream.range(1, 5));
     };
 
 
     }
-
